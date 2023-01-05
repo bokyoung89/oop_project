a62f95157d698c938b8edf198865ed67ed4bb1ba
sbk8689 <sbk8689@gmail.com>
2023-01-05 16:28:36 +0900

    feat : 양수일 때만 계산할 수 있다. (0 또는 음수일 때는 IllegalArgument 예외 발생)

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -27,33 +28,25 @@ public class CalculatorTest {
     @ParameterizedTest
     @MethodSource("formulaAndResult")
     void calculateTest(int operand1, String operator, int operand2, int result) {
-        int calculatorResult = Calculator.calculate(operand1, operator, operand2);
+        int calculatorResult = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
 
         assertThat(calculatorResult).isEqualTo(result);
     }
 
-    private static Stream<Arguments> formulaAndResult(){
+    private static Stream<Arguments> formulaAndResult() {
         return Stream.of(
-                arguments(1,"+",2,3),
-                arguments(1,"-",2,-1),
-                arguments(4,"*",2,8),
-                arguments(4,"/",2,2)
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(4, "*", 2, 8),
+                arguments(4, "/", 2, 2)
         );
     }
 
     @DisplayName("뺄셈 연산을 수행한다.")
     @Test
     void subtractionTest() {
-        int result = Calculator.calculate(1, "-", 2);
+        int result = Calculator.calculate(new PositiveNumber(1), "-", new PositiveNumber(2));
 
         assertThat(result).isEqualTo(-1);
-    }
-
-    @DisplayName("나눗셈에서 0을 나누는 경우 IllegalArgumentException을 발생시킨다.")
-    @Test
-    void calculateExceptionTest() {
-        assertThatCode(() -> Calculator.calculate(10,"/", 0))
-                .isInstanceOf((IllegalArgumentException.class))
-                .hasMessage("0으로는 나눌 수 없습니다.");
     }
 }
