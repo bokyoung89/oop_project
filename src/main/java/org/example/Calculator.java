235f19137d3dac36022cc865533c734f019904d0
sbk8689 <sbk8689@gmail.com>
2023-01-05 15:52:32 +0900

    refactor : interface를 구현하여 객체지향적 리팩토링

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.example.calculate.*;
+
+import java.util.List;
+
 public class Calculator {
+
+    public static final List<NewArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplcationOperator(), new DivisionOperator());
+
     public static int calculate(int operand1, String operator, int operand2) {
-        return ArithemticOperator.calculate(operand1, operator, operand2);
+        return arithmeticOperators.stream()
+                .filter(arithmeticOperator -> arithmeticOperator.supports(operator))
+                .map(arithmeticOperator -> arithmeticOperator.calculate(operand1, operand2))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
     }
 }
