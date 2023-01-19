c031ddfa177bfb2625764029e478f32de1a2bbe5
sbk8689 <sbk8689@gmail.com>
2023-01-19 15:12:53 +0900

    feat: 손님 객체 구현

---
+++ src/main/java/org/example/Cook/Customer.java
@@ -0,0 +1,10 @@
+package org.example.Cook;
+
+public class Customer {
+
+    public void order(String menuName, Menu menu, Cooking cooking){
+        MenuItem menuItem = menu.choose(menuName);
+        Cook cook = cooking.makeCook(menuItem);
+
+    }
+}
