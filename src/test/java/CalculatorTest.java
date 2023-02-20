c5785dbbaf7ff0ca43a8a5e33bc9492b46db17a8
sbk8689 <sbk8689@gmail.com>
2023-02-20 21:06:42 +0900

    test : 다른 인자들로 반복 테스트하기 (PramaterizedTest 어노테이션 사용)

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,38 +1,30 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class CalculatorTest {
-    @DisplayName("덧셈 연산을 수행한다.")
-    @Test
-    void additionTest() {
-        int result = Calculator.Calculate(1,"+",2);
+    @DisplayName("사칙연산을 수행한다.")
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void calculateTest(int operand1, String operator, int operand2, int result) {
+        int calculateResult = Calculator.Calculate(operand1, operator, operand2);
 
-        assertThat(result).isEqualTo(3);
+        assertThat(calculateResult).isEqualTo(result);
     }
 
-    @DisplayName("뺼셈 연산을 수행한다.")
-    @Test
-    void minusTest() {
-        int result = Calculator.Calculate(1,"-",2);
-
-        assertThat(result).isEqualTo(-1);
-    }
-
-    @DisplayName("곱셈 연산을 수행한다.")
-    @Test
-    void multipleTest() {
-        int result = Calculator.Calculate(2,"*",2);
-
-        assertThat(result).isEqualTo(4);
-    }
-
-    @DisplayName("나눗셈 연산을 수행한다.")
-    @Test
-    void divideTest() {
-        int result = Calculator.Calculate(6,"/",2);
-
-        assertThat(result).isEqualTo(3);
+    private static Stream<Arguments> formulaAndResult(){
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(2, "*", 2, 4),
+                arguments(6, "/", 2, 3)
+        );
     }
 }
