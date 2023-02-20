9798d8f8663326566028fce6130c8a7ef99e04b8
sbk8689 <sbk8689@gmail.com>
2023-02-20 21:35:40 +0900

    refactor : enum 클래스 생성하여 추상메소드 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,14 +1,5 @@
 public class Calculator {
     public static int Calculate(int operand1, String operator, int operand2) {
-        if(operator.equals("+")){
-            return operand1 + operand2;
-        } else if (operator.equals("-")) {
-            return operand1 - operand2;
-        } else if (operator.equals("*")) {
-            return operand1 * operand2;
-        } else if (operator.equals("/")) {
-            return operand1 / operand2;
-        }
-        return 0;
+        return ArithmeticOperator.calculate(operand1, operator, operand2);
     }
 }
