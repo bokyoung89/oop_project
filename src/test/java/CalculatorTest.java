f20a141cf9367ef8f5914285b904af85ee12bef7
sbk8689 <sbk8689@gmail.com>
2023-03-04 17:39:02 +0900

    refactor : 인터페이스로 선언하여 공통 메서드 구현

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,6 +9,13 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+/*
+* 요구사항
+* 간단한 사칙연산을 할 수 있다.
+* 양수로만 계산할 수 있다.
+* 나눗셈에서 0을 나누는 경우 IllegalArgumentException 예외를 발생시킨다.
+* MVC패턴 기반으로 구현한다.
+*/
 public class CalculatorTest {
     @DisplayName("사칙연산을 수행한다.")
     @ParameterizedTest
