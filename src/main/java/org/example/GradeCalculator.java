9bb4d96bf81cea7c7df8d9e9ada9004f028ba2f2
sbk8689 <sbk8689@gmail.com>
2023-03-07 14:15:59 +0900

    refactor : getter로 정보를 가져와서 평균학점을 계산하는 방법 대신 Course 객체에게 메시지를 던져 작업하게끔 개선

--- src/main/java/org/example/GradeCalculator.java
+++ src/main/java/org/example/GradeCalculator.java
@@ -22,7 +22,7 @@ public class GradeCalculator {
         // (학점수 * 교과목 평점)의 합계
         double multipliedCreditAndCourseGrade = 0;
         for (Course course : courses) {
-            multipliedCreditAndCourseGrade += course.getCredit() * course.getGradeToNumber();
+            multipliedCreditAndCourseGrade += course.multiplyCreditAndCourseGrade();
         }
         // 수강신청 총학점 수
         int totalCompletedCredit = courses.stream()
@@ -31,3 +31,5 @@ public class GradeCalculator {
         return multipliedCreditAndCourseGrade / totalCompletedCredit;
     }
 }
+
+
